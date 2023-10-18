5bced092bfbc8f4a968a2592cf8444cabaa62f68
Bahpu <51274837+Bahpu@users.noreply.github.com>
2023-10-18 21:15:11 +0100

    Creating inventory front end page

--- clickergame/src/App.js
+++ clickergame/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import MainContainer from "./components/maincontainer/maincontainer";
 import Dashboard from "./Pages/dashboard/dashboard";
 import Login from "./Pages/login/login";
+import Inventory from "./Pages/inventory/inventory";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainContainer />}></Route>
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/inventory" element={<Inventory />} />
     </Routes>
   );
 }
