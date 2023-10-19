bf76d33e4be75cbf725356614fbc985486635bd7
Bahpu <51274837+Bahpu@users.noreply.github.com>
2023-10-19 15:11:02 +0100

    Front end for register page

--- clickergame/src/App.js
+++ clickergame/src/App.js
@@ -3,11 +3,13 @@ import MainContainer from "./components/maincontainer/maincontainer";
 import Dashboard from "./Pages/dashboard/dashboard";
 import Login from "./Pages/login/login";
 import Inventory from "./Pages/dashboard/inventory/inventory";
+import Register from "./Pages/register/register";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainContainer />}></Route>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/inventory" element={<Inventory />} />
     </Routes>
