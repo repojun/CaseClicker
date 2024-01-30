feff65faf1d302029afcf6b64913a767ada73294
Arjun <ZKAC319@live.rhul.ac.uk>
2024-01-30 15:40:55 +0000

    Fixing file structures

--- src/components/testbutton/testbutton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./testbutton.module.css";
-import React  from 'react';
-const TestButton = ({ title, children, className = " ", ...props }) => {
-  return (
-    <div className={styles.box} onClick={props.click}>  &nbsp; </div>
-  );
-};
-
-export default TestButton;
