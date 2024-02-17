efd9cb95e1b640d888b8a1b699f905c1428c4bc0
Arjun <ZKAC319@live.rhul.ac.uk>
2024-02-17 13:42:13 +0000

    Adding temporary backgrounds

--- src/Pages/dashboard/store/store.jsx
+++ src/Pages/dashboard/store/store.jsx
@@ -10,20 +10,19 @@ function Store() {
         <>
             <MainContainer>
                 <SubContainer>
-                    <div className={styles.centerTitle}>
-                        <Header title="Store"> </Header>
-                    </div>
-                    <div className={styles.itemContainer}>
-                        <ShopItem price="$450.00" title="Random Item" image="/recoilcasenew.png" />
-                        <ShopItem price="$1000.00" title="Normal Item" image="/dreamnightmaresnew.png" />
-                        <ShopItem price="$100.00" title="Wordy Item" image="/brokenfangnew.png" />
-                    </div>
-                    <div className={styles.itemContainer}>
-                        <ShopItem price="$15,000.00" title="Big Word Item" image="/brokenfangnew.png" />
-                        <ShopItem price="$750.00" title="New Word Item" image="/recoilcasenew.png" />
-                        <ShopItem price="$250.00" title="Testing Item" image="/dreamnightmaresnew.png" />
-                    </div>
-
+                        <div className={styles.centerTitle}>
+                            <Header title="Store"> </Header>
+                        </div>
+                        <div className={styles.itemContainer}>
+                            <ShopItem price="$450.00" title="Random Item" image="/recoilcasenew.png" />
+                            <ShopItem price="$1000.00" title="Normal Item" image="/dreamnightmaresnew.png" />
+                            <ShopItem price="$100.00" title="Wordy Item" image="/brokenfangnew.png" />
+                        </div>
+                        <div className={styles.itemContainer}>
+                            <ShopItem price="$15,000.00" title="Big Word Item" image="/brokenfangnew.png" />
+                            <ShopItem price="$750.00" title="New Word Item" image="/recoilcasenew.png" />
+                            <ShopItem price="$250.00" title="Testing Item" image="/dreamnightmaresnew.png" />
+                        </div>
                 </SubContainer>
             </MainContainer>
         </>
