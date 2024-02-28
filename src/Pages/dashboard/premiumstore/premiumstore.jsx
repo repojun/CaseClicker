560a529c3054ce3013fc12f8c1e5a394b298f88d
Arjun <ZKAC319@live.rhul.ac.uk>
2024-02-28 12:20:45 +0000

    Setting up skeleton files

---
+++ src/Pages/dashboard/premiumstore/premiumstore.jsx
@@ -0,0 +1,14 @@
+import styles from "./premiumstore.module.css"
+import maincontainer from "../../../components/maincontainer/maincontainer"
+import SubContainer from "../../../components/subcontainer/subcontainer"
+import Maincontainer from "../../../components/maincontainer/maincontainer";
+
+function PremiumStore() {
+<Maincontainer>
+    <SubContainer>
+        
+    </SubContainer>
+</Maincontainer>
+}
+
+export default observer(PremiumStore);
