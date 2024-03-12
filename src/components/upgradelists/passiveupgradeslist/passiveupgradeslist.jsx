dd54333e950ed0353193703e552700ac096e7e65
Arjun <ZKAC319@live.rhul.ac.uk>
2024-03-12 23:16:32 +0000

    Messy version of passive upgrades functioning

--- src/components/upgradelists/passiveupgradeslist/passiveupgradeslist.jsx
+++ src/components/upgradelists/passiveupgradeslist/passiveupgradeslist.jsx
@@ -6,26 +6,61 @@ import Axios from "../../../api/agent";
 
 const PassiveUpgradesList = (
   {
-  multiplierFunction,
-  frontendArray,
-  moneyFunction,
-  props,
-}) => {
-  const {userStore: {user, setBalance, setPassiveUpgrade}} = useContextStore()
+    multiplierFunction,
+    frontendArray,
+    moneyFunction,
+    props,
+  }) => {
+  const { userStore: { user, setBalance, setPassiveUpgrade } } = useContextStore()
 
   useEffect(() => {
-    if (user && typeof user.balance !== 'undefined') {
+    if (user && typeof user.balance !== 'undefined' && user.passiveUpgrade1 == 1) {
       const interval = setInterval(async () => {
-        var newBalanceVariable = user.balance + 5;
+        var newBalanceVariable = user.balance + 0.01;
         setBalance(newBalanceVariable);
-        const query = await Axios("/api/user/setbalance", "POST", {balance:newBalanceVariable});
+        const query = await Axios("/api/user/setbalance", "POST", { balance: newBalanceVariable });
       }, 5000); // 5000 milliseconds = 5 seconds
-  
       return () => clearInterval(interval);
     }
+
   }, [user]);
 
+  useEffect(() => {
+    if (user && typeof user.balance !== 'undefined' && user.passiveUpgrade2 == 1) {
+      const interval = setInterval(async () => {
+        var newBalanceVariable = user.balance + 0.02;
+        setBalance(newBalanceVariable);
+        const query = await Axios("/api/user/setbalance", "POST", { balance: newBalanceVariable });
+      }, 4000); // 5000 milliseconds = 5 seconds
+      return () => clearInterval(interval);
+    }
+    
+  }, [user]);
 
+  
+  useEffect(() => {
+    if (user && typeof user.balance !== 'undefined' && user.passiveUpgrade3 == 1) {
+      const interval = setInterval(async () => {
+        var newBalanceVariable = user.balance + 0.04;
+        setBalance(newBalanceVariable);
+        const query = await Axios("/api/user/setbalance", "POST", { balance: newBalanceVariable });
+      }, 2000); // 5000 milliseconds = 5 seconds
+      return () => clearInterval(interval);
+    }
+    
+  }, [user]);
+
+  useEffect(() => {
+    if (user && typeof user.balance !== 'undefined' && user.passiveUpgrade3 == 1) {
+      const interval = setInterval(async () => {
+        var newBalanceVariable = user.balance + 0.08;
+        setBalance(newBalanceVariable);
+        const query = await Axios("/api/user/setbalance", "POST", { balance: newBalanceVariable });
+      }, 1000); // 5000 milliseconds = 5 seconds
+      return () => clearInterval(interval);
+    }
+    
+  }, [user]);
 
   const clickCheck = async (e, price, isBought, ID) => {
     var Xlocation = e.clientX;
@@ -34,9 +69,9 @@ const PassiveUpgradesList = (
     console.log(isBought);
     if (user.balance >= price && isBought == 0) {
       setBalance(user.balance);
-      moneyFunction(price); 
+      moneyFunction(price);
       setPassiveUpgrade(ID, 1)
-      const query = await Axios("/api/user/setpassiveupgrade", "POST", {passiveUpgradeID: ID})
+      const query = await Axios("/api/user/setpassiveupgrade", "POST", { passiveUpgradeID: ID })
     } else {
       console.log(user.balance);
       var newElement = ( // Stores div in variable to be stored in useStateArray
@@ -57,8 +92,8 @@ const PassiveUpgradesList = (
         // REMOVE ARRAY ITEMS HERE
       }, 3000);
     }
-    
-    
+
+
   };
 
   const upgradeBoxes = [
@@ -70,11 +105,11 @@ const PassiveUpgradesList = (
   const upgradeList = [
     upgradeBoxes.map((keyname) => {
       var priceChecker = user.balance >= keyname.price ? "#15e815" : "red";
-      
+
       if (keyname.isBought) {
         return null;
       }
-      
+
       return (
         <div
           style={{
