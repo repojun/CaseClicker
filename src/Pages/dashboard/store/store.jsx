3bdb007a97a1b31a240d01495742c64cf3d9ae5c
Arjun <ZKAC319@live.rhul.ac.uk>
2024-03-19 13:30:03 +0000

    Redoing frontend to make it more modular

--- src/Pages/dashboard/store/store.jsx
+++ src/Pages/dashboard/store/store.jsx
@@ -6,27 +6,42 @@ import styles from "./store.module.css";
 import ShopItem from "../../../components/shopitem/shopitem";
 
 function Store() {
+    const shopItems = [
+        { ID: 1, price: 450, title: "Random Item", image: "/recoilcasenew.png" },
+        { ID: 2, price: 1000, title: "Normal Item", image: "/dreamnightmaresnew.png" },
+        { ID: 3, price: 100, title: "Wordy Item", image: "/brokenfangnew.png" },
+        { ID: 4, price: 15000, title: "Big Word Item", image: "/brokenfangnew.png" },
+        { ID: 5, price: 750, title: "New Word Item", image: "/recoilcasenew.png" },
+        { ID: 6, price: 250, title: "Testing Item", image: "/dreamnightmaresnew.png" }
+    ];
+
+    function chunkArray(array, size) {
+        const chunkedArr = [];
+        for (let i = 0; i < array.length; i += size) {
+            chunkedArr.push(array.slice(i, i + size));
+        }
+        return chunkedArr;
+    }
+    const itemChunks = chunkArray(shopItems, 3);
+
     return (
         <>
             <MainContainer>
                 <SubContainer>
-                        <div className={styles.centerTitle}>
-                            <Header title="Store"> </Header>
+                    <div className={styles.centerTitle}>
+                        <Header title="Store" />
+                    </div>
+                    {itemChunks.map((chunk, index) => (
+                        <div key={index} className={styles.itemContainer}>
+                            {chunk.map((item, mapIndex) => (
+                                <ShopItem key={mapIndex} price={item.price} title={item.title} image={item.image} />
+                            ))}
                         </div>
-                        <div className={styles.itemContainer}>
-                            <ShopItem price="$450.00" title="Random Item" image="/recoilcasenew.png" />
-                            <ShopItem price="$1000.00" title="Normal Item" image="/dreamnightmaresnew.png" />
-                            <ShopItem price="$100.00" title="Wordy Item" image="/brokenfangnew.png" />
-                        </div>
-                        <div className={styles.itemContainer}>
-                            <ShopItem price="$15,000.00" title="Big Word Item" image="/brokenfangnew.png" />
-                            <ShopItem price="$750.00" title="New Word Item" image="/recoilcasenew.png" />
-                            <ShopItem price="$250.00" title="Testing Item" image="/dreamnightmaresnew.png" />
-                        </div>
+                    ))}
                 </SubContainer>
             </MainContainer>
         </>
     );
 }
 
-export default observer(Store);
+export default observer(Store); 
