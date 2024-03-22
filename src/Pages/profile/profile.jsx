151077bf3e4262ac77d5ef204e0ff06cc4d9de28
Bahpu <51274837+Bahpu@users.noreply.github.com>
2024-03-22 15:37:07 +0000

    Improving profile page

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -1,16 +1,31 @@
 import { React, useContext } from "react";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom"; 
+import { useParams } from "react-router-dom";
 import useContextStore from "../../context";
+import styles from "./profile.module.css";
+import MainContainer from "../../components/maincontainer/maincontainer";
+import SubContainer from "../../components/subcontainer/subcontainer";
 
 function Profile() {
   const {
     userStore: { user },
-  } = useContext(useContextStore()); 
+  } = useContextStore();
 
-  let { username } = useParams(); 
+  let { username } = useParams();
 
-  return <div className={styles.profile}>{username}</div>; 
+  return (
+    <MainContainer>
+      <SubContainer>
+
+          <div className={styles.flex}>
+            <div className={styles.subContainer}>
+              <div className={styles.subContainerTitle}>{username}</div>
+            </div>
+          </div>
+
+      </SubContainer>
+    </MainContainer>
+  );
 }
 
 export default observer(Profile);
