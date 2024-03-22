0db0fc32cf7c00f72a8deb5f55c6daadf774c912
Bahpu <51274837+Bahpu@users.noreply.github.com>
2024-03-22 23:58:39 +0000

    Setting up paypal functions

---
+++ src/components/paypal/paypal.jsx
@@ -0,0 +1,38 @@
+import {PayPalScriptProvider, PayPalButtons} from "@paypal/react-paypal-js";
+import createPayment from "./create";
+
+export default function PayPal({total, userId, openModal, closeModal}) {
+  const onApprove = (actions) => {
+    return actions.order
+      .capture()
+      .then(() => {
+        openModal();
+      })
+      .catch(() => {
+        openModal();
+      });
+  };
+
+  return (
+    <PayPalScriptProvider
+      options={{
+        "client-id": process.env.REACT_PUBLIC_PAYPAL_CLIENT_ID,
+        currency: "USD",
+        intent: "capture",
+      }}
+    >
+      <PayPalButtons
+        style={{layout: "vertical"}}
+        forceReRender={[total, userId]}
+        createOrder={(_, actions) => {
+          const p = createPayment({
+            total: total,
+            user: userId,
+          });
+          return actions.order.create(p);
+        }}
+        onApprove={(_, actions) => onApprove(actions)}
+      />
+    </PayPalScriptProvider>
+  );
+}
