b8dbd002ade9a320ce40c56aba1466cce1f9f96e
Bahpu <51274837+Bahpu@users.noreply.github.com>
2024-03-22 14:47:54 +0000

    Clearing white space before moving branch

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,16 +8,11 @@ const setPassiveUpgrade = require("../middleware/user/setpassiveupgrade");
 const setPassiveUpgradeLevel = require("../middleware/user/setpassiveupgradelevel");
 const setItem = require("../middleware/user/setitem");
 
-
-
 router.get("/exists", userExists);
 router.post("/setbalance", setBalance);
 router.post("/setpremiumbalance", setPremiumBalance);
 router.post("/setpassiveupgrade", setPassiveUpgrade);
 router.post("/setpassiveupgradelevel", setPassiveUpgradeLevel);
 router.post("/setitem", setItem);
-
-
-
 
 module.exports = router;
