318760d4b85f6780612bb32353d4b26fb5d9e9d2
Bahpu <51274837+Bahpu@users.noreply.github.com>
2024-03-23 00:23:54 +0000

    Functioning paypal butotn

--- src/components/paypal/paypal.jsx
+++ src/components/paypal/paypal.jsx
@@ -1,7 +1,7 @@
-import {PayPalScriptProvider, PayPalButtons} from "@paypal/react-paypal-js";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import createPayment from "./createpayment";
 
-export default function PayPal({total, userId, openModal, closeModal}) {
+export default function PayPal({ total, userId, openModal, closeModal }) {
   const onApprove = (actions) => {
     return actions.order
       .capture()
@@ -12,23 +12,24 @@ export default function PayPal({total, userId, openModal, closeModal}) {
         openModal();
       });
   };
+  console.log("CLIENT_ID IS HERE: " + process.env.REACT_APP_PAYPAL_CLIENT_ID);
 
   return (
     <PayPalScriptProvider
       options={{
-        "client-id": process.env.REACT_PUBLIC_PAYPAL_CLIENT_ID,
+        "client-id": "AfYO-NUinS8gZluLLm0YTGcP9CzOqxp2lgoIRcuFl81cLGqK1rNYi5uATNaxYUVu9RACsJHcDmqlM119",
         currency: "USD",
         intent: "capture",
       }}
     >
       <PayPalButtons
-        style={{layout: "vertical"}}
         forceReRender={[total, userId]}
         createOrder={(_, actions) => {
           const p = createPayment({
             total: total,
             user: userId,
           });
+          console.log("P IS HERE: " + p);
           return actions.order.create(p);
         }}
         onApprove={(_, actions) => onApprove(actions)}
