773d1c4127dc04a16758ab5a450fcfc4c0cf8b39
Arjun <ZKAC319@live.rhul.ac.uk>
2024-03-27 00:09:10 +0000

    Creating first case

--- src/components/caseopener/caseopener.jsx
+++ src/components/caseopener/caseopener.jsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import useContextStore from "../../context";
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 import styles from "./caseopener.module.css";
 
 const CaseOpener = () => {
@@ -9,24 +9,43 @@ const CaseOpener = () => {
     } = useContextStore();
 
     const items = [];
+    const rarityChances = {
+        'Contraband': 0.0025,    // 0.25%
+        'Ultra Rare': 0.005,      // 0.5%
+        'Rare': 0.02,             // 2%
+        'Uncommon': 0.0975,       // 9.75%
+        'Common': 0.1875          // 18.75%
+    };
 
-    for (const key in user.inventory) {
-      const item = user.inventory[key];
-  
-      if (item.purchasable == 0) {
-        for (let i = 0; i < 1; i++) {
-          items.push({
-            title: item.viewname,
-            price: item.price,
-            image: item.image,
-            rarity: item.rarity,
-            entityName: item.entname,
-          });
-          
+    const case1 = [
+        'karambit_fade',
+        'glock_fade',
+        'deagle_codered',
+        'mp7_bloodsport',
+        'scar20_emerald',
+        'usp_stainless',
+        'awp_suninleo',
+        'deagle_mudder',
+        'glock_candyapple',
+        'mp7_gunsmoke'
+    ];
+
+    case1.forEach(entname => {
+        const item = user.inventory[entname];
+        if (item && item.purchasable === 0) {
+            const rarity = item.rarity;
+            const chance = rarityChances[rarity];
+            items.push({
+                title: item.viewname,
+                price: item.price,
+                image: item.image,
+                rarity: rarity,
+                entityName: entname,
+                chance: chance,
+            });
         }
-      }
-    }
-  
+    });
+
     return (
         <div onClick={() => console.log(items)}>hey</div>
     )
